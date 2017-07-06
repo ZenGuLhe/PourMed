44f5f0e380bacfb5f4f27c4d0b76d1162048767b
gulhe <guillaume.lhermitte@zenika.com>
2017-07-06 16:35:56 +0200

    init

---
+++ src/main/java/org/test/camunda/springboot/config/CamundaConfig.java
@@ -0,0 +1,6 @@
+package org.test.camunda.springboot.config;
+
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+
+@EnableProcessApplication
+public class CamundaConfig {}
